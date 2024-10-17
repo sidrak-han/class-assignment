c73f42cfdda8ea8086de6488487d2253732b4515
sidrak-han <sid23560065@gmail.com>
2024-10-17 11:07:02 +0500

    Add files via upload

---
+++ thursday assigment2/nextjs/src/app/components/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header(){
+    return(
+        <>
+        <div className="header">
+            <h1>LOGO</h1>
+            <ul>
+               <li>HOME</li>
+               <li>ABOUT US</li>
+               <li>CONTACT</li>
+            </ul>
+            </div>
+            </>
+    )
+}
